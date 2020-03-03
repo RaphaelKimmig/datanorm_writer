2529d00388f623a3b2138fda153790f5edb50b95
Raphael Kimmig <raphael.kimmig@ampad.de>
2020-03-03 09:38:00 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="datanorm_writer",
+    version="0.0.1",
+    url="https://github.com/RaphaelKimmig/datanorm_writer",
+    author="Raphael Kimmig",
+    author_email="raphael@ampad.de",
+    description="A library for writing datanorm files",
+    include_package_data=True,
+    packages=find_packages(),
+)
